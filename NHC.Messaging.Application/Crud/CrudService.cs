8b4992e0fe56edd0b5cc4f4a51178db27ff5d9a9
Amany Elmorsy <mostafa.ahmed.elfeky@gmail.com>
2022-04-23 03:54:52 +0200

    remove unused libs

--- NHC.Messaging.Application/Crud/CrudService.cs
+++ NHC.Messaging.Application/Crud/CrudService.cs
@@ -2,11 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using NHC.Messaging.Application.Exceptions;
 using NHC.Messaging.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NHC.Messaging.Application.Crud
 {
